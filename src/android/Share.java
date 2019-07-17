7f1b06d90186a0c82da12fb02b7abeef63a096a5
Asanga Abeywickrama <Asanga.Abeywickrama@retailzoo.com.au>
2019-07-17 20:31:57 +1000

    init

--- src/android/Share.java
+++ src/android/Share.java
@@ -1,4 +1,4 @@
-package cordova-plugin-share-me;
+package cordovaPluginShareMe;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -37,6 +37,6 @@ import android.content.Intent;
             } catch(Error e) {
                 callbackContext.error(e.getMessage());
             }
-            
+
         }
     }
